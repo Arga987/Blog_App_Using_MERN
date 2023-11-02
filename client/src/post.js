0007ee4e8ea7189fcfa339e76964f8298b270ade
Arun <arunadiga48@gmail.com>
2023-11-02 22:24:31 +0530

    Displaying posts from db

--- client/src/post.js
+++ client/src/post.js
@@ -1,16 +1,19 @@
-export default function Post() {
+
+import {format} from "date-fns"
+
+export default function Post({title,summary,cover,content,createdAt}) {
     return(
         <div className="post">
         <div className="image">
           <img src="https://img.freepik.com/free-photo/man-neon-suit-sits-chair-with-neon-sign-that-says-word-it_188544-27011.jpg?size=626&ext=jpg&ga=GA1.1.386372595.1697932800&semt=ais" alt="Image for test"></img>
         </div>
         <div className="texts">
-          <h2>A gamer playing games and being lost in its world</h2>
+          <h2>{title}</h2>
           <p className="info">
             <a className="author">Arun S Adiga</a>
-            <time>2023-10-22 18:10</time>
+            <time>{format(new Date(createdAt), 'MMM d, yyyy HH:mm')}</time>
           </p>
-          <p className="summary">A dog is a man's best friend</p>
+          <p className="summary">{summary}</p>
         </div>
       </div>
     );
